ca1558d641b05b09e47c97671c214b64a673f72f
Phil Lucks <lucksp@gmail.com>
2016-02-21 22:05:19 -0700

    working set

--- js/apresCntrlr.js
+++ js/apresCntrlr.js
@@ -7,17 +7,37 @@ app.controller('apresController', ['$scope', 'mtnFactory', function($scope, mtnF
 			response.data.forEach(function(item){
 				mtnFactory.getWeather(item.zip).then(weatherPull)
 				$scope.mtnData.push(item)
-				console.log('MtnData:', $scope.mtnData)
+				console.log('MtnData:', item.zip)
 			})
 		}
 		mtnFactory.getMtns().then(mtnPull)
 
+
+// THIS VERSION PULLS WEATHER DATA FROM WWONLINE
+		// $scope.weatherData = []
+		// var weatherPull = function(response){
+		// 	response.data.data.weather.forEach(function(item){
+		// 		$scope.weatherData.push(item)
+		// 		console.log('weatherData', $scope.weatherData)
+		// 	})
+		// }
+// THIS VERSION PULLS WEATHER DATA FROM WUNDERGROUND
 		$scope.weatherData = []
 		var weatherPull = function(response){
-			console.log(respose.data.data)
-			response.data.data.weather.forEach(function(item){
-				$scope.weatherData.push(item)
-				console.log('weatherData', $scope.weatherData)
-			})
+			// response.data.current_observation.forEach(function(item) {
+				$scope.weatherData.push(response.data)
+				console.log($scope.weatherData)
+			// console.log(response.data)
+			// })
 		}
+
+// THIS VERSION PULLS NEAREST_AREA DATA FROM WWONLINE
+		// $scope.otherMtnData = []
+		// var weatherPull = function(response){
+		// 	response.data.data.nearest_area.forEach(function(item){
+		// 		console.log(response.data)
+		// 		$scope.otherMtnData.push(item)
+		// 		console.log('weatherData', $scope.otherMtnData)
+		// 	})
+		// }
 }]);
--- js/mtnFactory.js
+++ js/mtnFactory.js
@@ -6,11 +6,14 @@ app.factory('mtnFactory', ['$http', function($http){
 	return {
 
 		getMtns : function(){
-			return $http.get('./js/mtnData.json')
+			return $http.get('/js/mtnData.json');
 			},
-		getWeather : function(zipcode){
-			return $http.get('https://api.worldweatheronline.com/free/v2/ski.ashx?key=3c9dedea970c88b460a8a65f876db&q='+zipcode+'&format=json&includelocation=yes')
-			}
+		// getWeather : function(zipcode){
+		// 	return $http.get('https://api.worldweatheronline.com/free/v2/ski.ashx?key=3c9dedea970c88b460a8a65f876db&q='+zipcode+'&format=json&includelocation=yes')
+		// 	}
+		getWeather: function(zipcode){
+			return $http.get('http://api.wunderground.com/api/af3980c1176ac12e/conditions/forecast/settings/q/'+zipcode+'.json')
+		}
 	}
 
 }]);
